05668ff0dcf6348ae6f0a1703451b6df6f3d5e61
Sg <bolajisegun2015@gmail.com>
2023-11-05 12:41:37 +0100

    Zillionaire laundry website

---
+++ JS/schedule.js
@@ -0,0 +1,82 @@
+// schedule pickup page start here
+let pickupPlan = document.querySelector('.One-Time-Pickup ');
+let login = document.querySelector('.login');
+let loginForm = document.querySelector('.login-form');
+let regularPickup = document.querySelector('.Regular-Pickup');
+let longTermPickup = document.querySelector('.Long-Term-Pickup');
+
+pickupPlan.addEventListener('click', function(){
+    login.style.display='none';
+    loginForm.style.display='block';
+});
+regularPickup.addEventListener('click', function(){
+    login.style.display='none';
+    loginForm.style.display='block';
+});
+longTermPickup.addEventListener('click', function(){
+    login.style.display='none';
+    loginForm.style.display='block';
+});
+// show previous page
+let prevPage = document.querySelector('.prevPage')
+
+prevPage.addEventListener('click', function(){
+    login.style.display='block';
+    loginForm.style.display='none';
+})
+// zillionairelaundry@gmail.com
+// Email message by users 
+function sendToEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "getgetget@gmail.com",
+        Password : "62F8AEBA21A98F0BC5A900207154B4FE6E3E",
+        To : 'zillionairelaundry@gmail.com',
+        From : "getgetget@gmail.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
+
+// function sendToMail(){
+//     let sendMail ={
+//         name: document.getElementById('name').value,
+//         phoneNo: document.getElementById('phoneNo').value,
+//         address: document.getElementById('pickupAddress').value,
+//         plan: document.getElementById('plans').value
+//     }
+//     const serviceId = "service_v0fi7us";
+//     const templateId = "template_q9xfdsk";
+
+//     emailjs.send(serviceId,templateId,sendMail).then( res =>{
+//             document.getElementById('name').value = "";
+//             document.getElementById('phoneNo').value= "";
+//             document.getElementById('pickupAddress').value= "";
+//             document.getElementById('plans').value= "";
+//             console.log(res)
+//             alert("message send");
+//         })
+//         .catch((err) => console.log(err))
+// }
+
+// function sendToEmail(){
+//     var name = document.getElementById('name').value;
+//     var email = document.getElementById('email').value;
+//     var phoneNo = document.getElementById('phoneNo').value;
+//     var pickupAddress = document.getElementById('pickupAddress').value;
+//     var plans = document.getElementById('plans').value;
+
+//     windows.location.href = "mailto:bolajisegun2015@gmail.com?name="+name+"&email="+email+"&phoneNo="+phoneNo+"&pickupAddress="+pickupAddress+"&plans="+plans;
+// }
+// schedule pickup page ends here
+
+// preloader
+let loader = document.getElementById('preloader');
+
+window.addEventListener('load', function(){
+    window.setTimeout(function(){
+        loader.style.display = 'none';
+    }, 3000);
+});
---
+++ JS/script.js
@@ -0,0 +1,81 @@
+// preloader
+let loader = document.getElementById('preloader');
+
+window.addEventListener('load', function(){
+    window.setTimeout(function(){
+        loader.style.display = 'none';
+    }, 70);
+    
+});
+
+// typing effect for hero section 
+var typingText = document.getElementById('typing-text')
+var typingText2 = document.getElementById('text2')
+var index = 0;
+var indexs = 0;
+var texts = "ZILLIONAIRE ";
+var text2 = "Never spend your weekends catching up on laundry again. We come get your laundry on demand, giving you 3+ extra hours to spend however you want."
+
+function typeText(){
+    
+    if (index < texts.length) {
+        typingText.textContent += texts.charAt(index);
+        index++;
+        setTimeout(typeText, 250)
+       }       
+    
+}
+typeText()
+// second text
+function typeText2(){
+    
+    if (indexs < text2.length) {
+        typingText2.textContent += text2.charAt(indexs);
+        indexs++;
+        setTimeout(typeText2, 20)
+       }    
+} 
+typeText2()
+
+
+let ourStory = document.querySelector('.our-story');
+let aboutUs = document.querySelectorAll('.about-us');
+
+ourStory.addEventListener('click', function(){
+    aboutUs.style.display = 'initial';
+})
+
+let navBtn = document.querySelector('.nav-btn');
+let closeBtn = document.querySelector('.close-nav-btn');
+let mobileLinks = document.querySelector('.mobile-navLinks');
+
+navBtn.addEventListener('click', function(){
+    mobileLinks.classList.add('show-links');
+    navBtn.style.display = 'none';
+    closeBtn.style.display = 'initial';
+});
+closeBtn.addEventListener('click', function(){
+    mobileLinks.classList.remove('show-links')
+    navBtn.style.display = 'initial';
+    closeBtn.style.display = 'none';
+});
+
+// animate on scroll using gsap 
+let animation = gsap.to('animatedScroll', {
+    x:200,
+    duration:1,
+})
+
+let animatedScroll = document.querySelector('.sec-one');
+let scrollAni = gsap.timeline({
+    scrollTrigger: {
+        trigger: 'animatedScroll',
+        start: 'top center',
+        end: 'bottom center',
+        scrub: 'true',
+        marker: 'false'
+    }
+});
+
+gsap.registerPlugin(scrollAni);
+
